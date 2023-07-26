54d985b865514d087edc4dcacc23c5891d09d73a
Woen-Sug Choi <woensug.choi@gmail.com>
2023-07-27 08:23:10 +0900

    color change

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -31,7 +31,7 @@ export type BotProps = {
     fontSize?: number
 }
 
-const defaultWelcomeMessage = 'Hi there! How can I help?'
+const defaultWelcomeMessage = '안녕하세요. 무엇을 도와드릴까요?'
 
 /*const sourceDocuments = [
     {
